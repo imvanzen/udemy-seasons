8081cffff0af41e280954fd4b8b2923601b7a373
J.Reczko <reczko.jakub@gmail.com>
2022-06-15 21:08:09 +0200

    Season congig

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: 'Lets hit the beach',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Brr, it is chilly',
+    iconName: 'snowflake'
+  }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter'
@@ -10,14 +21,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth())
-  const text = season === 'winter' ? 'Brr, it is chilly' : 'Lets hit the beach'
-  const icon = season === 'winter' ? 'snowflake' : 'sun'
+  const {text, iconName } = seasonConfig[season]
 
   return (
     <div>
-      <i className={`icon ${icon}`} />
+      <i className={`icon ${iconName}`} />
       <h1>{ text }</h1>
-      <i className={`icon ${icon}`} />
+      <i className={`icon ${iconName}`} />
     </div>
   );
 };
